47ba1955e0b1d9300178012155a5a787ccf4f6cd
Connor Helsing <conhelsing@gmail.com>
2019-03-05 20:54:05 -0700

    general

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,39 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// jQuery + dependencies
+import JQuery from 'jquery'
+import Velocity from 'velocity-animate'
+//
+
+import Rails from 'rails-ujs'
+import Turbolinks from 'turbolinks'
+import ActionCable from 'actioncable'
+import * as ActiveStorage from 'activestorage'
+
+import _ from 'underscore'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
+function importAll (r) {
+  r.keys().forEach(r)
+}
+
+window.$ = JQuery
+window._ = _
+
+import 'datatables.net-se'
+import 'datatables.net-responsive-se'
+// import Chart from 'chart.js'
+
+importAll(require.context('../lib/', true, /\.js$/))
+importAll(require.context('../marketing/', true, /\.js$/))
+importAll(require.context('../main/', true, /\.js$/))
+importAll(require.context('../portal_blocks/', true, /\.js$/))
+
+window.App = {}
+App.cable = ActionCable.createConsumer()
